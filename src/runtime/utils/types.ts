565f855fb54bdbe088aeeb9fd333567a384ff910
germondai <bruno.krocek@gmail.com>
2024-12-21 02:28:57 +0100

    forgot to add notification type (utils/types)

---
+++ src/runtime/utils/types.ts
@@ -0,0 +1,17 @@
+export type Enumize<T extends string> = { [K in T]: K }
+
+export type NotificationType =
+  | 'INFO'
+  | 'SUCCESS'
+  | 'WARNING'
+  | 'ERROR'
+  | 'SYSTEM'
+
+export const NotificationType: Enumize<NotificationType> = {
+  INFO: 'INFO',
+  SUCCESS: 'SUCCESS',
+  WARNING: 'WARNING',
+  ERROR: 'ERROR',
+  SYSTEM: 'SYSTEM',
+} as const
+
