0db39d8081f539f2a3641a8f2a0f38405a10aa7b
germondai <bruno.krocek@gmail.com>
2024-12-21 02:40:16 +0100

    TypeWriter component

---
+++ src/runtime/components/TypeWriter.vue
@@ -0,0 +1,85 @@
+<template>
+  <span>
+    <span :class="writerClass" v-html="typeValue" />
+    <span
+      class="inline-block w-1 ml-1 bg-white animate-[cursorBlink_1s_infinite]"
+      :class="{ 'animate-none': typeStatus }"
+      >&nbsp;</span
+    >
+  </span>
+</template>
+
+<script lang="ts" setup>
+import {
+  type HTMLAttributes,
+  onBeforeUnmount,
+  onMounted,
+  ref,
+  watch,
+} from 'vue'
+
+const {
+  typeArray,
+  writerClass,
+  typingSpeed = 200,
+  erasingSpeed = 100,
+  newTextDelay = 2000,
+} = defineProps<{
+  writerClass?: HTMLAttributes['class']
+  typeArray: string[]
+  typingSpeed?: number
+  erasingSpeed?: number
+  newTextDelay?: number
+}>()
+
+const typeValue = ref<string>('')
+const typeStatus = ref<boolean>(false)
+
+let typeArrayIndex = 0
+let charIndex = 0
+let typingTimeout: ReturnType<typeof setTimeout>
+
+const typeText = () => {
+  if (charIndex < typeArray[typeArrayIndex]!.length) {
+    typeStatus.value = true
+
+    typeValue.value += typeArray[typeArrayIndex]!.charAt(charIndex)
+    charIndex++
+
+    typingTimeout = setTimeout(typeText, typingSpeed)
+  } else {
+    typeStatus.value = false
+    typingTimeout = setTimeout(eraseText, newTextDelay)
+  }
+}
+
+const eraseText = () => {
+  if (charIndex > 0) {
+    typeStatus.value = true
+
+    typeValue.value = typeArray[typeArrayIndex]!.substring(0, charIndex - 1)
+    charIndex--
+
+    typingTimeout = setTimeout(eraseText, erasingSpeed)
+  } else {
+    typeStatus.value = false
+    charIndex = 0
+    typeArrayIndex = (typeArrayIndex + 1) % typeArray.length
+    typingTimeout = setTimeout(typeText, typingSpeed + 1000)
+  }
+}
+
+const resetTyping = () => {
+  typeArrayIndex = 0
+  charIndex = 0
+  typeValue.value = ''
+  clearTimeout(typingTimeout)
+  typeText()
+}
+
+watch(() => typeArray, resetTyping, { deep: true })
+
+onMounted(() => resetTyping())
+
+onBeforeUnmount(() => clearTimeout(typingTimeout))
+</script>
