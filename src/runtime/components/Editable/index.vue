38381b5e576c21ee5b2cdfae4bf6cce750c33578
germondai <bruno.krocek@gmail.com>
2024-12-31 20:49:07 +0100

    fixed editable colors

--- src/runtime/components/Editable/index.vue
+++ src/runtime/components/Editable/index.vue
@@ -2,7 +2,7 @@
   <Primitive v-bind="props" class="group/EditableWrapper relative">
     <button
       v-if="allowed"
-      class="absolute top-2 right-2 flex items-center gap-1.5 text-gray-200/60 hover:text-gray-200 opacity-0 group-hover/EditableWrapper:opacity-100 transition-all z-20"
+      class="absolute top-2 right-2 flex items-center gap-1.5 text-primary-50/80 hover:text-primary-50 opacity-0 group-hover/EditableWrapper:opacity-100 transition-all z-20"
       @click="value = !value"
     >
       <Icon :name="value ? 'mdi:pencil-off' : 'mdi:pencil'" class="size-5" />
