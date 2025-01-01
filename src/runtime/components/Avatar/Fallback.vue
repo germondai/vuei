f740b4cd47dea0d3bbba30536698efe7eaf48d7c
germondai <bruno.krocek@gmail.com>
2025-01-01 21:03:37 +0100

    AvatarFallback

---
+++ src/runtime/components/Avatar/Fallback.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { injectAvatarContext } from './index.vue'
+import type { PrimitiveProps } from '../../../module'
+import Primitive from '../Primitive/index.vue'
+
+const { ...props } = defineProps<{} & PrimitiveProps>()
+
+const { imageLoadingStatus } = injectAvatarContext()
+</script>
+
+<template>
+  <Primitive
+    v-if="imageLoadingStatus !== 'loaded'"
+    v-bind="props"
+    class="size-full flex items-center justify-center leading-none"
+  >
+    <slot />
+  </Primitive>
+</template>
