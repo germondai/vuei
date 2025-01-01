e2a384491299f717f0cd4de9e4ef057a8f6cf54e
germondai <bruno.krocek@gmail.com>
2025-01-01 21:03:28 +0100

    AvatarImage

---
+++ src/runtime/components/Avatar/Image.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { injectAvatarContext } from './index.vue'
+import type { PrimitiveProps } from '../../../module'
+import Primitive from '../Primitive/index.vue'
+import { NuxtImg } from '#components'
+
+const { as = NuxtImg, asChild, ...props } = defineProps<{} & PrimitiveProps>()
+
+const { imageLoadingStatus } = injectAvatarContext()
+</script>
+
+<template>
+  <Primitive
+    v-show="imageLoadingStatus === 'loaded'"
+    :as
+    :asChild
+    v-bind="props"
+    role="img"
+    class="size-full rounded-[inherit] object-cover"
+    @load="imageLoadingStatus = 'loaded'"
+    @error="imageLoadingStatus = 'error'"
+  >
+    <slot />
+  </Primitive>
+</template>
